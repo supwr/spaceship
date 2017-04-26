f9c592da2ffc7db5748165a7fbabc91e2e7f755b
Marcelo Rodrigues <marcelo.rodriguespires@gmail.com>
2017-04-26 20:48:15 +0000

    started room subscription

--- src/Services/Chat.php
+++ src/Services/Chat.php
@@ -7,30 +7,46 @@ use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
     protected $clients;
+    protected $rooms = array();
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->rooms["rooms"] = new \SplObjectStorage;
     }
 
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
+        $this->rooms[uniqid()] = new \SplObjectStorage;
 
+        $this->onSubscribe($conn, "rooms");
         echo "New connection! ({$conn->resourceId})\n";
     }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
-        $data = json_decode($msg,true);
-        $message = $data["message"];
 
+    public function onSubscribe(ConnectionInterface $conn, $room){
+      $this->rooms["rooms"]->attach($conn);    
+    }
+
+    protected function broadcast($topic, $msg) {
+        foreach ($this->rooms[$topic] as $client) {
+            $client->send($msg);
+        }
+    }
+
+
+    public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
+
+        $msgData = json_decode($msg);
+
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $message, $numRecv, $numRecv == 1 ? '' : 's');
+            , $from->resourceId, $msgData->message, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->clients as $client) {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
-                $client->send($message);
+                $client->send($msgData->message);
             }
         }
     }
